cd011423960e09bc48d58b3a4f5d4518efb12bd5
LucaPalminteri <lucapalminteri02@gmail.com>
2023-12-19 21:09:29 -0300

    feat: add skills and projects

---
+++ src/components/Projects/Project.tsx
@@ -0,0 +1,13 @@
+
+export default async function Projects() {
+  return (
+    <div>
+      <h3 className="font-medium text-2xl pb-4">Projects</h3>
+      <ul>
+        <li><span className="font-semibold">Chrome extension dark-theme-jira</span>: A simple browser extension that enhances the user experience by providing a dark theme for Jira, making extended work sessions easier on the eyes.</li>
+        <li><span className="font-semibold">npm package supabase-repository</span>: Developed an npm package to streamline a common development task, improving efficiency and code organization for the wider development community.</li>
+        <li><span className="font-semibold">Web game crossword puzzle</span>: Created an interactive crossword puzzle game with a user-friendly interface, offering an enjoyable and challenging experience for puzzle enthusiasts</li>
+      </ul>
+    </div>
+  )
+}
---
+++ src/components/Skills/Skills.tsx
@@ -0,0 +1,16 @@
+export default async function Skills() {
+    return (
+        <div className="py-12">
+            <h3 className="font-medium text-2xl pb-4">Skills</h3>
+            <ul>
+                <li><span className="font-medium">Programming Languages</span>: JavaScript, TypeScript, Python, C#</li>
+                <li><span className="font-medium">Web Development</span>: React, Next.js, Redux, React Native, SCSS, Tailwind, Figma</li>
+                <li><span className="font-medium">Database</span>: MySQL, PostgreSQL, SQL Server, Redis, MongoDB</li>
+                <li><span className="font-medium">DevOps</span>: Docker, Kubernetes, Linux, GitLab CI, Digital Ocean</li>
+                <li><span className="font-medium">Communication Protocols</span>: RESTful APIs, gRPC</li>
+                <li><span className="font-medium">Project Management</span>: SCRUM, Jira</li>
+                <li><span className="font-medium">.NET Technologies</span>: Entity Framework, ASP.NET, WinForm</li>
+            </ul>
+        </div>
+    )
+}
