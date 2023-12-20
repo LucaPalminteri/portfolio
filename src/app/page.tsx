f6510f5aebc0a1de4a500e9f8a2324ae6b5fef92
LucaPalminteri <lucapalminteri02@gmail.com>
2023-12-19 21:08:51 -0300

    feat: add skills and projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import AboutMe from "../components/AboutMe/AboutMe";
+import Projects from "../components/Projects/Project";
+import Skills from "../components/Skills/Skills";
 
 export default async function Home() {
   return (
-    <main className="max-w-2xl mx-auto md:mt-10 p-4 md:p-8 lg:p-12">
+    <main className="max-w-3xl mx-auto md:mt-0 p-4 md:p-8 lg:p-12">
       <AboutMe/>
+      <Skills/>
+      <Projects/>
     </main>
   )
 }
